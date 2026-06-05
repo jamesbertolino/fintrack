c1ab868daa4bf4f340d42a0b8c340f801f607015
jamesbertolino <jamilbertolino@gmail.com>
2026-06-05 09:13:49 -0300

    fix: sincroniza VERSION do service worker com package.json (2.60.0)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION       = '2.59.0'
+const VERSION       = '2.60.0'
 const CACHE_STATIC  = `poupaup-static-${VERSION}`
 const CACHE_PAGES   = `poupaup-pages-${VERSION}`
 const STATIC_ASSETS = ['/', '/dashboard', '/login', '/logo.png', '/manifest.webmanifest']
