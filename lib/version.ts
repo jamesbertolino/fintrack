1413fd1574482aa1efc8b5869a2b15796b3d4145
jamesbertolino <jamilbertolino@gmail.com>
2026-06-05 09:18:47 -0300

    fix: atualiza APP_VERSION para 2.60.0 em lib/version.ts

--- lib/version.ts
+++ lib/version.ts
@@ -1,5 +1,5 @@
-export const APP_VERSION = '2.47.0'
-export const APP_BUILD   = '2026.06.03'
+export const APP_VERSION = '2.60.0'
+export const APP_BUILD   = '2026.06.05'
 
 /**
  * Histórico de versões
